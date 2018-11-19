c307802638a3882f520572f019357634912d9ec9
eramisetti <eramisetti@gmail.com>
2018-11-18 20:43:28 -0800

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # cc
+
+Install Java 8
+Install Maven updated version
+Install Eclipse updated version
+Install Maria Database- Create tables and insert data if any
+
+Open CCApplication.java and run as java application and application starts with http://localhost:8080
+Webservice would be available now.
--- src/main/java/com/cc/CCApplication.java
+++ src/main/java/com/cc/CCApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringFoundationsApplication {
+public class CCApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringFoundationsApplication.class, args);
+		SpringApplication.run(CCApplication.class, args);
 	}
 }
